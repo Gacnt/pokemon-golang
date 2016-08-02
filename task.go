846906cebdeacb1721c5bb00fe1614e06f08dfa9
Donovan Burton <gacnt@shaw.ca>
2016-08-01 20:39:07 -0600

    task priority works

--- task.go
+++ task.go
@@ -1,9 +1,16 @@
 package pgo
 
 import (
-	"log"
+	"sort"
 	"sync"
 	"time"
+)
+
+const (
+	Low    int = 1
+	Med    int = 2
+	High   int = 3
+	Urgent int = 4
 )
 
 // Job .
@@ -23,12 +30,19 @@ type Task struct {
 }
 
 type JobToRun struct {
-	ID  string
-	Job Job
+	ID       string
+	Job      Job
+	Priority int
 }
 
 // FuncJob .
 type FuncJob func()
+
+type Priority []JobToRun
+
+func (p Priority) Len() int           { return len(p) }
+func (p Priority) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p Priority) Less(i, j int) bool { return p[i].Priority > p[j].Priority }
 
 // Run the function task
 func (f FuncJob) Run() { f() }
@@ -37,18 +51,39 @@ func (f FuncJob) Run() { f() }
 // These tasks will be processed in the order they are received
 // taskType is optional, leaving the taskType blank will treat it as low priority
 // taskType can be "URGENT" "HIGH" "MED" "LOW"
-func (t *Task) AddFunc(id string, cmd func(), taskType ...string) {
+func (t *Task) AddFunc(id string, cmd func(), taskType ...int) {
 	if len(taskType) == 0 {
-		taskType = []string{""}
+		taskType = []int{1} // Task is Low priority
 	}
 	t.Mu.Lock()
 	defer t.Mu.Unlock()
-	if len(t.pending) <= t.MaxTasks {
-		t.pending = append(t.pending, JobToRun{id, FuncJob(cmd)})
-	} else if len(t.pending) >= t.MaxTasks && taskType[0] == "URGENT" {
-		t.pending = append(t.pending, JobToRun{id, FuncJob(cmd)})
+	highestTaskPriority := 0
+	for _, pendingTasks := range t.pending {
+		if pendingTasks.Priority > highestTaskPriority {
+			highestTaskPriority = pendingTasks.Priority
+		}
 	}
 
+	if len(t.pending) <= t.MaxTasks {
+		// Pending isn't full, add this task
+		t.pending = append(t.pending, JobToRun{id, FuncJob(cmd), taskType[0]})
+	} else if len(t.pending) >= t.MaxTasks && taskType[0] > highestTaskPriority {
+		// This task is more important than one of the tasks in the pending,
+		// remove the task and add this one instead
+		lowestPriorityTask := 4
+		lowestPriorityTaskI := 0
+		for i, pendingTasks := range t.pending {
+			if pendingTasks.Priority < lowestPriorityTask {
+				lowestPriorityTask = pendingTasks.Priority
+				lowestPriorityTaskI = i
+			}
+		}
+		// Remove lower priority task
+		t.pending = append(t.pending[:lowestPriorityTaskI], t.pending[lowestPriorityTaskI+1:]...)
+		// Add new task
+		t.pending = append(t.pending, JobToRun{id, FuncJob(cmd), taskType[0]})
+
+	}
 	// Else throw away the task
 }
 
@@ -66,17 +101,17 @@ func (t *Task) run() {
 	for stop := false; !stop; {
 		select {
 		case <-time.Tick(2 * time.Second):
-			log.Println(t.tasks)
+			sort.Sort(Priority(t.tasks))
 			for len(t.tasks) > 0 {
 				t.tasks[0].Job.Run()
 				t.tasks = t.tasks[1:]
 				time.Sleep(3 * time.Second)
 			}
 			t.Mu.Lock()
+			sort.Sort(Priority(t.pending))
 			for _, pendingJob := range t.pending {
 				found := false
 				for _, taskedJobs := range t.tasks {
-					log.Println(pendingJob.ID, taskedJobs.ID)
 					if pendingJob.ID == taskedJobs.ID {
 						found = true
 						break
